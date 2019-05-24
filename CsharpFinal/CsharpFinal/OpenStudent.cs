3d6304863d9b426e1bd1b41a509e04e3975f3577
Daniel <danielgo512@gmail.com>
2019-05-24 12:52:38 -0700

    create Json student infomation

---
+++ CsharpFinal/CsharpFinal/OpenStudent.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpFinal
+{
+
+
+
+    public class Student : Person, IUpdatatable
+    {
+        public override void Greeting()
+        {
+            string separator = new string('#', 80);
+            Console.WriteLine(separator);
+            base.Greeting();
+            Console.WriteLine($"I have started {Program} at {Start_Date.ToString("MMMM dd, yyyy")} in ITD Canada \n" +
+                    $"I am able to develop code in the following programing langeuages");
+            foreach (var item in Skillset)
+            {
+                Console.Write(item.Key+" ");
+
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine($"Updated : {Update_Date.ToString("MMMM dd, yyyy")}");
+            Console.WriteLine(separator);
+            Console.WriteLine("\n");
+        }
+        public Dictionary<string, bool> Skillset { get; set; }
+        public string Program { get; set; }
+        public DateTime Start_Date { get; set; }
+        public DateTime Update_Date { get; set; }
+    }
+
+    public interface  IUpdatatable
+    {
+
+        DateTime Update_Date { get; set; }
+
+    }
+
+    public abstract class Person
+    {
+        public string Name { get; set; }
+        public string Last_Name { get; set; }
+        public string Email { get; set; }
+
+        public virtual void Greeting()
+        {
+            Console.Write($"Hello, I am {Name} {Last_Name}, \n" +
+            $"My email is {Email}\n");
+        }
+     
+    }
+}
+
+    
--- CsharpFinal/CsharpFinal/Program.cs
+++ CsharpFinal/CsharpFinal/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using Newtonsoft.Json;
+
 
 namespace CsharpFinal
 {
@@ -6,7 +11,27 @@ namespace CsharpFinal
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            finalExam();
+
+        }
+
+        public static void finalExam()
+        {
+            WebClient client = new WebClient();
+
+          string result = client.DownloadString("http://www.json-generator.com/api/json/get/bPCtVMPmNu?indent=0");
+          Student[] letMEgoHome = JsonConvert.DeserializeObject<Student[]>(result);
+            for (int i = 0; i < letMEgoHome.Length; i++)
+            {
+                letMEgoHome[i].Greeting();
+            }
+
+           
+
+        
+
+
+
         }
     }
 }
